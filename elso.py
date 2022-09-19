728ca604900d55960121ed52e635fd7d212a4bca
Matyi444 <jojart.matyas@szbiszeged.hu>
2022-09-19 10:36:36 +0200

    elsocommit

--- elso.py
+++ elso.py
@@ -1,4 +1,12 @@
 from os import system
-import string
 
 system("cls")
+
+# 2.feladat prog_alapf.3
+
+#explicit típuskonverzió
+szam = (input("Kérek egy számot: "))
+kiir = f"A szám: {szam} és kétszerese: {2 * szam}"
+
+print(kiir)
+
